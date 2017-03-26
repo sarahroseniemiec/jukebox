b7925f26fcde069677a4b223c2bbaf3e621cbd66
sarah niemiec <sroseniemiec@gmail.com>
2017-03-26 18:43:53 -0400

    finished homework

--- main.js
+++ main.js
@@ -5,6 +5,9 @@ var stopButton = document.querySelector("#stop")
 var nextButton = document.querySelector("#next")
 var backButton = document.querySelector("#back")
 var newSongButton = document.querySelector("#newsong")
+var youRock = document.querySelector("#yourock")
+var rockWith = document.querySelector("#rockwith")
+var steveBiko = document.querySelector("#stevebiko")
 
 var index = [0]
 var titleSpot = document.querySelector("#titlespot")
@@ -66,7 +69,7 @@ Jukebox.prototype.back = function () {
   this.songs[index].currentTime = 0
   index --
   if (index < 0) {
-      index = 0
+      index = this.songs.length -1
   }
   this.songs[index].play()
 }
@@ -80,16 +83,25 @@ playButton.addEventListener("click", function(event){
   titleSpot.innerHTML = titles[index]
   artistSpot.innerHTML = artists[index]
   jukebox.play()
+  playButton.style.color = "#ff4f5a"
+  pauseButton.style.color = "#2d2d2d"
+  stopButton.style.color = "#2d2d2d"
 })
 
 pauseButton.addEventListener("click", function(event){
   event.preventDefault()
   jukebox.pause()
+  pauseButton.style.color = "#ff4f5a"
+  playButton.style.color = "#2d2d2d"
+  stopButton.style.color = "#2d2d2d"
 })
 
 stopButton.addEventListener("click", function(event) {
   event.preventDefault()
   jukebox.stop()
+  stopButton.style.color = "#ff4f5a"
+  playButton.style.color = "#2d2d2d"
+  pauseButton.style.color = "#2d2d2d"
 })
 
 nextButton.addEventListener("click", function(event){
@@ -97,6 +109,9 @@ nextButton.addEventListener("click", function(event){
   jukebox.forward()
   titleSpot.innerHTML = titles[index]
   artistSpot.innerHTML = artists[index]
+  playButton.style.color = "#ff4f5a"
+  pauseButton.style.color = "#2d2d2d"
+  stopButton.style.color = "#2d2d2d"
 })
 
 backButton.addEventListener("click", function(event){
@@ -104,6 +119,9 @@ backButton.addEventListener("click", function(event){
   jukebox.back()
   titleSpot.innerHTML = titles[index]
   artistSpot.innerHTML = artists[index]
+  playButton.style.color = "#ff4f5a"
+  pauseButton.style.color = "#2d2d2d"
+  stopButton.style.color = "#2d2d2d"
 })
 
 newSongButton.addEventListener("click", function(event) {
@@ -117,4 +135,37 @@ newSongButton.addEventListener("click", function(event) {
   songs.push(newSong)
   titles.push(newTitle)
   artists.push(newArtist)
+})
+
+youRock.addEventListener("click", function(event){
+  event.preventDefault()
+  var youRockMy = new Audio("audiofiles/YouRockMyWorld.mp3")
+  var youRockmyTitle = "Title: " + "You Rock My World"
+  var youRockmyArtist = "Artist: " + "Micheal Jackson"
+  songs.push(youRockMy)
+  titles.push(youRockmyTitle)
+  artists.push(youRockmyArtist)
+  youRock.style.color = "#ff4f5a"
+})
+
+rockWith.addEventListener("click", function(event){
+  event.preventDefault()
+  var rockWithYou = new Audio("audiofiles/rockwithyou.mp3")
+  var rockWithYouTitle = "Title: " + "Rock With You"
+  var rockWithYouArtist = "Artist: " + "Micheal Jackson"
+  songs.push(rockWithYou)
+  titles.push(rockWithYouTitle)
+  artists.push(rockWithYouArtist)
+  rockWith.style.color = "#ff4f5a"
+})
+
+steveBiko.addEventListener("click", function(event){
+  event.preventDefault()
+  var steve = new Audio("audiofiles/SteveBiko(StirItUp).mp3")
+  var steveTitle = "Title: " + "Steve Biko (Stir It Up)"
+  var steveArtist = "Artist: " + "A Tribe Called Quest"
+  songs.push(steve)
+  titles.push(steveTitle)
+  artists.push(steveArtist)
+  steveBiko.style.color = "#ff4f5a"
 })
